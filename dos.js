176a896be679a8770418ac7eb3bd1bb6e6629d4c
mplisak <59884139+mplisak@users.noreply.github.com>
2020-06-08 20:35:02 +0200

    Add files via upload

--- dos.js
+++ dos.js
@@ -9,60 +9,64 @@ c.addEventListener("mousedown", fillPixel);
 const ctx = c.getContext("2d");
 ctx.scale(sc, sc);  
 
-var tableColumns = new Array(140); 
-  
-for (var i = 0; i < tableColumns.length; i++) { 
-    tableColumns[i] = []; 
-}
+var tableOne = new Array(140); 
+var tableTwo = new Array(140);
 
-for (var i = 0; i < tableColumns.length; i++) { 
-    for (var j = 0; j < tableColumns.length; j++) { 
-        tableColumns[i][j] = "0"; 
-    } 
-} 
+function createTable(someArray) {
+	for (var i = 0; i < someArray.length; i++) { 
+		someArray[i] = []; 
+	}
+	for (var i = 0; i < someArray.length; i++) { 
+		for (var j = 0; j < someArray.length; j++) { 
+			someArray[i][j] = "0"; 
+		} 
+	} 
+}
+ 
+createTable(tableOne);
+createTable(tableTwo);
 
 function fillPixel(event) {
-	var x = Math.floor((event.clientX - canvas.offsetLeft - 5)/sc);
-	var y = Math.floor((event.clientY - canvas.offsetTop - 5)/sc);
-	if(tableColumns[x][y] == "0"){
-		ctx.fillRect(x, y, 1, 1);
-		tableColumns[x][y] = "1";
-	}else{
-		ctx.clearRect(x, y, 1, 1);
-		tableColumns[x][y] = "0";
+	if (sGame == 0) {
+		var x = Math.floor((event.clientX - canvas.offsetLeft - 5)/sc);
+		var y = Math.floor((event.clientY - canvas.offsetTop - 5)/sc);
+		if (tableOne[x][y] == "0") {
+			ctx.fillRect(x, y, 1, 1);
+			tableOne[x][y] = "1";
+		}else{
+			ctx.clearRect(x, y, 1, 1);
+			tableOne[x][y] = "0";
+		}
 	}
-
 }
-
-
 
 function pauseGame() {
-	if(pGame == 0 && sGame == 1) {
-		pGame = 1;
-		document.getElementById("b1").innerHTML = "resume";
-	}else{
-		pGame = 0;
-		document.getElementById("b1").innerHTML = "pause";
+	if (sGame == 1) {
+		if (pGame == 0) {
+			pGame = 1;
+			document.getElementById("b1").innerHTML = "resume";
+		}else{
+			pGame = 0;
+			document.getElementById("b1").innerHTML = "pause";
+			startGame();
+		}
 	}
 }
-
-
 
 function resetGame(){
 	sGame = 0;
 	pGame = 0;
 	document.getElementById("b1").innerHTML = "pause";
-	for (var i = 0; i < tableColumns.length; i++) { 
-		for (var j = 0; j < tableColumns.length; j++) { 
-			tableColumns[i][j] = "0"; 
+	for (var i = 0; i < tableOne.length; i++) { 
+		for (var j = 0; j < tableOne.length; j++) { 
+			tableOne[i][j] = "0"; 
 		} 
 	} 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 }
 
 function startGame() {
-	sGame = 1;
+	if (pGame == 0) {
+		sGame = 1;
+	}
 }
-
-
